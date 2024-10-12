281123b78fac7bd26f756c29a2d29eea48733752
Federico Arias <federicoarias.engineering@gmail.com>
2024-10-12 16:57:21 -0500

    Prueba para manejo de datos de la API con Gson

--- src/main/java/com/icaro/fedeexchange/main/Main.java
+++ src/main/java/com/icaro/fedeexchange/main/Main.java
@@ -5,19 +5,24 @@
 package com.icaro.fedeexchange.main;
 
 import com.icaro.fedeexchange.controler.Base;
-
+import com.google.gson.JsonObject;
 /**
  *
  * @author USUARIO
  */
 public class Main {
+    JsonObject jo = new JsonObject();
     
     public static void main(String[] args) {
     Base base = new Base();
-        System.out.println(base.leerBase());
+        System.out.println(base.leerBase());        
+        System.out.println(base.leerBase().get("conversion_rates").getClass());
+        System.out.println((base.leerBase().getAsJsonObject("conversion_rates").get("COP").getAsDouble()) );
         System.out.println(base.leerCodigos());
-    
-    
+        System.out.println(base.leerCodigos().getClass());
+        System.out.println(base.leerCodigos().get("supported_codes").getClass());
+         System.out.println(base.leerCodigos().getAsJsonArray("supported_codes").get(0).getAsJsonArray().get(1) );
+        
     }
     
 }
